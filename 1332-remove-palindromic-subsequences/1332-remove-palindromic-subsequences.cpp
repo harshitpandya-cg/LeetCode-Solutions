0be46c3b0316518fd33d7da0fd4cf12442529ecc
Harshit Pandya <harshit.pandya.cg@gmail.com>
2026-05-28 15:44:40 +0530

    Time: 0 ms (100%), Space: 8.2 MB (76.07%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while(left < right) {   
+            if(s[left] != s[right]) {
+                return 2;
+            }
+            left++;
+            right--;
+        }
+        return 1;
+    }
+};
