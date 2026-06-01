65ff651e8a9b48ad4d4ebafbfd46458ec2ef590c
Harshit Pandya <harshit.pandya.cg@gmail.com>
2026-06-01 15:35:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (44%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+
+        string hexChars = "0123456789abcdef";
+        string result = "";
+
+        unsigned int n = num;
+
+        while (n > 0) {
+            result = hexChars[n % 16] + result;
+            n /= 16;
+        }
+
+        return result;
+    }
+};
