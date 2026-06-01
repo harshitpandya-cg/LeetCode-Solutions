b118ae2441d3ed46ac64517a114dfbaa3b83a236
Harshit Pandya <harshit.pandya.cg@gmail.com>
2026-06-01 15:37:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (67.96%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result = x ^ y;
+        int count = 0;
+
+        while (result) {
+            count += result & 1;
+            result >>= 1;
+        }
+
+        return count;
+    }
+};
