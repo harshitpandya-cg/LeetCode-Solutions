b79f16b210e5d14a77ae4b75f415b762b1649acf
Harshit Pandya <harshit.pandya.cg@gmail.com>
2026-06-06 13:09:33 +0530

    Time: 0 ms (100%), Space: 31.2 MB (98.9%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,8 +3,10 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
+
             if (nums[mid] == target) {
                 return mid;
             }
@@ -15,6 +17,7 @@ public:
                 right = mid - 1;
             }
         }
+
         return -1;
     }
 };
